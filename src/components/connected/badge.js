a986a47730b54390cb996e786fedc6cac9c3569d
Ogunderu Noah Ife <ifeoluwadayonoah@gmail.com>
2022-09-05 07:25:03 +0100

    badge mobile fix

--- src/components/connected/badge.js
+++ src/components/connected/badge.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Badge = ({leftIcon, children, rightIcon, paid, unpaid}) => {
   return (
-    <div className={`${unpaid ? 'bg-vencru-lightred': 'bg-vencru-lightgreen'} flex items-center justify-center rounded-full w-2/4 py-[3px] px-2 `}>
+    <div className={`${unpaid ? 'bg-vencru-lightred': 'bg-vencru-lightgreen'} flex items-center justify-center rounded-full w-5/7 lg:w-2/4 py-[3px] px-2 `}>
       {!!leftIcon && <span className={`${unpaid ? 'text-vencru-red': 'text-vencru-green'} text-xs pr-2 `}>{leftIcon}</span>}
       <span className={`${paid ? 'text-vencru-darkgreen' : 'text-vencru-red'}`}>{children}</span>
       {!!rightIcon && <span className=''>{rightIcon}</span>}
